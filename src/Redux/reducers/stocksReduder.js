fb880f7612d9a2e183118e79f582126489525966
docwali777 <drgauvin@mgmcare.org>
2017-09-13 12:07:33 -0400

    react-chartjs

--- src/Redux/reducers/stocksReduder.js
+++ src/Redux/reducers/stocksReduder.js
@@ -7,15 +7,21 @@ export function stocksReducer(state = [], action){
   switch(action.type){
     case 'FIND_STOCK':
 let stocksFounds = {...state}
-
+// {x: Date.parse(d.timestamp), y: d.close}
 let data = action.payload.data.map((d, i)=>{
-  return {x: Date.parse(d.timestamp), y: d.close}
+  return {x: new Date(Date.parse(d.timestamp)).toLocaleString().split(',')[0], y: d.close}
 })
 let newState = [...state, {
-  name: action.payload.name,
-  values: data,
-  strokeWidth: 3
+  label: action.payload.name,
+  data,
+  fill: false,
+  backgroundColor: 'red',
+  // pointBorderColor: 'green',
+  borderColor: 'purple',
+  borderWidth: 1,
+  radius: 1
   }]
+  console.log('NEW_STATE', newState);
   socket.emit('display', {stocks: newState})
     return newState
     break;
